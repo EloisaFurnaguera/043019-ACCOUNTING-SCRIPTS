355a0d5dddd16c10e1e07c8c5a38788bc570ed4d
vagrant <vagrant@vagrant.vm>
2019-05-07 05:23:01 +0000

    Melon Detail Report

---
+++ melon_info_1.py
@@ -0,0 +1,18 @@
+"""Print out all the melons in our inventory."""
+
+
+from melons_1 import melons
+
+
+def print_melon(melons):
+    """Print each melon with corresponding attribute information."""
+
+    for melon_name, attributes in melons.items():
+        print(melon_name.upper())
+
+    for attribute, value in attributes.items():
+        print(f'{attribute}: {value}')
+
+      
+ 
+print_melon(melons)
